2ccce28a7dee6143ea00902c4bbc49ea1e759d34
viviayi <741173912@qq.com>
2020-02-25 20:59:05 +0800

    0225

--- postOrder.py
+++ postOrder.py
@@ -5,6 +5,7 @@ class Node:
         self.val = val
         self.children = children
 """
+#递归法
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         outList = []
@@ -16,3 +17,18 @@ class Solution:
             outList.extend(Solution.postorder(self,child))
         outList.append(root.val)
         return outList
+"""
+迭代法关键点：利用栈，每次将根节点出栈时，都将子节点压入栈（这样能保证一条子线遍历到结束）
+"""
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        outList = []
+        if not root:
+            return []
+        stack = [root,]
+        while stack:
+            _root = stack.pop()
+            outList.append(_root.val)
+            for child in _root.children:
+                stack.append(child)
+        return outList[::-1]
