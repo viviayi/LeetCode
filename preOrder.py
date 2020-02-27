44cd5a273193e98d32217ba9649b65c7da1fb2a1
viviayi <741173912@qq.com>
2020-02-27 19:29:01 +0800

    0227

--- preOrder.py
+++ preOrder.py
@@ -21,3 +21,21 @@ class Solution(object):
             outList.extend(Solution.preorder(self,child))
         return outList
         
+"""
+迭代法，每个节点依次遍历，需要先进先出，所以压栈需要逆向
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        outList = []
+        if not root:
+            return []
+        stack = [root,]
+        while stack:
+            _root = stack.pop()
+            outList.append(_root.val)
+            stack.extend(_root.children[::-1])
+        return outList
